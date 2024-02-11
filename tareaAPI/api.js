5e32f4bf7a320a2a406d222cec8e41838ecb9686
aldricHJA <aldrichmills@gmail.com>
2024-02-10 21:28:17 -0600

    trabajos en clase y tarea de api

--- tareaAPI/api.js
+++ tareaAPI/api.js
@@ -1,4 +1,4 @@
-fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
+fetch('https://pokeapi.co/api/v2/pokemon/')
   .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok');
@@ -7,25 +7,30 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
   })
   .then(data => {
     const { results } = data;
-    Promise.all(results.map(pokemon => fetch(pokemon.url).then(response => response.json())))
-      .then(pokemonsData => {
-        pokemonsData.forEach(pokemonData => {
-          const { name, abilities, stats } = pokemonData;
-          console.log(`Nombre: ${name}`);
+    results.forEach(pokemon => {
+      fetch(pokemon.url) 
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(pokemonData => {
+          console.log(`Nombre: ${pokemonData.name}`);
           console.log('Habilidades:');
-          abilities.forEach(({ ability }) => {
+          pokemonData.abilities.forEach(({ ability }) => {
             console.log(`- ${ability.name}`);
           });
           console.log('Estadísticas:');
-          stats.forEach(({ stat, base_stat }) => {
+          pokemonData.stats.forEach(({ stat, base_stat }) => {
             console.log(`- ${stat.name}: ${base_stat}`);
           });
           console.log('----------------------------------------');
+        })
+        .catch(error => {
+          console.error('Error fetching Pokémon data:', error);
         });
-      })
-      .catch(error => {
-        console.error('Error fetching Pokémon data:', error);
-      });
+    });
   })
   .catch(error => {
     console.error('Error fetching data:', error);
